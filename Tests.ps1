13fb7a2e937d4e9087949551ff737818bd7b74b1
Maciek Misztal <mmisztal1980@gmail.com>
2021-08-27 23:26:15 +0200

    initial microservice template (#2)
    * initial microservice template
    
    * tests added
    
    * per-svc suffix added
    
    * test reusability
    
    * 2nd test reusability
    
    * per-service invocation
    
    * display name updated
    
    * display names updated
    
    * workflow fixed
    
    * test results
    
    * directory creation forced

--- Tests.ps1
+++ Tests.ps1
@@ -1,59 +1,30 @@
 Import-Module "Pester";
+Import-Module "$PSScriptRoot/scripts/Ion-Tests.psm1" -Force;
 
-Describe -tag "E2ETest" -Name "ion-job" {
-  It "Should render template with solution for each <svc> service " -ForEach @(
+Describe -tag "job" -Name "ion-job tests" {
+  It "Should render template with solution for each service type: <svc>" -ForEach @(
     @{ "SVC" = "job" }
   ) {
-    
-    Remove-Item -Path "tests" -Recurse -Force -ErrorAction Ignore;
-    dotnet new -i "./$SVC"
-
-    New-Item -Name "tests" -ItemType "directory";
-
-    Push-Location "$PSScriptRoot/tests";
-
-    dotnet new ion-$SVC --project "Project" --service "Service"
-
-    Pop-Location;
-
-    Test-Path "tests/Project.sln" | Should -Be $true;
-    Test-Path "tests/.editorconfig" | Should -Be $true;
-    Test-Path "tests/LICENSE" | Should -Be $true;
-    Test-Path "tests/readme.md" | Should -Be $true;
-    Test-Path "tests/project-service-job" | Should -Be $true;
-
-    Test-Path "tests/.idea" | Should -Be $false;
+    Start-TemplateRenderingTest -Type $SVC -Solution $true -Root $PSScriptRoot;
   }
 
-  It "Should build the template with solution for each <svc> service " -ForEach @(
+  It "Should build the template with solution for each service <svc>" -ForEach @(
     @{ "SVC" = "job" }
   ) {
-    
-    Remove-Item -Path "tests" -Recurse -Force -ErrorAction Ignore;
-    & dotnet new -i "./$SVC"
+    Start-TemplateBuildTest -Type $SVC -Root $PSScriptRoot;
+  }
+}
 
-    New-Item -Name "tests" -ItemType "directory";
+Describe -tag "microservice" -Name "ion-microservice tests" {
+  It "Should render template with solution for each service type: <svc>" -ForEach @(
+    @{ "SVC" = "microservice" }
+  ) {
+    Start-TemplateRenderingTest -Type $SVC -Solution $true -Root $PSScriptRoot;
+  }
 
-    try {
-      Push-Location "$PSScriptRoot/tests";
-
-      & dotnet new ion-$SVC --project "Project" --service "Service"
-    } finally {
-      Pop-Location;
-    }
-
-    try {
-      Push-Location "$PSScriptRoot/tests/project-service-$SVC/src/Project.Service.Job";
-
-      & dotnet restore 
-
-      $LASTEXITCODE | Should -Be 0 -Because "dotnet restore should pass for $SVC";
-
-      & dotnet build --no-restore
-
-      $LASTEXITCODE | Should -Be 0 -Because "dotnet build should pass for $SVC";
-    } finally {
-      Pop-Location;
-    }
+  It "Should build the template with solution for each service <svc>" -ForEach @(
+    @{ "SVC" = "microservice" }
+  ) {
+    Start-TemplateBuildTest -Type $SVC -Root $PSScriptRoot;
   }
 }
---
+++ scripts/Ion-Tests.psm1
@@ -0,0 +1,89 @@
+Import-Module "Pester";
+
+[hashtable]$suffix = @{
+  "job"          = "Job"
+  "microservice" = "Svc"
+};
+
+function Start-TemplateRenderingTest {
+  param(
+    [string]$Type,
+    [bool]$Solution = $true,
+    [string]$Root
+  )
+
+  Remove-Item -Path "$Root/tests/$Type" -Recurse -Force -ErrorAction Ignore;
+  dotnet new -i "$Root/$Type"
+
+  Push-Location $Root;
+  New-Item -Name "tests/$Type" -ItemType "directory" -Force;
+  Pop-Location;
+
+  Push-Location "$Root/tests/$Type";
+
+  if ($true -eq $solution) {
+    dotnet new ion-$Type --project "Project" --service "Service"
+
+    Test-Path "$Root/tests/$Type/Project.sln" | Should -Be $true -Because ".sln file should exist";
+    Test-Path "$Root/tests/$Type/.editorconfig" | Should -Be $true -Because ".editoconfig file should exist";
+    Test-Path "$Root/tests/$Type/LICENSE" | Should -Be $true -Because "license file should exist";
+    Test-Path "$Root/tests/$Type/readme.md" | Should -Be $true -Because "readme.md should exist";
+    Test-Path "$Root/tests/$Type/project-service-$($suffix[$Type].ToString().ToLowerInvariant())" | Should -Be $true -Because "service folder should exist";
+  }
+  else {
+    dotnet new ion-$Type --project "Project" --service "Service" --solution false
+
+    Test-Path "$Root/tests/$Type/Project.sln" | Should -Be $false -Because ".sln file should not exist";
+    Test-Path "$Root/tests/$Type/.editorconfig" | Should -Be $false -Because ".editoconfig file should not exist";
+    Test-Path "$Root/tests/$Type/LICENSE" | Should -Be $false -Because "license file should not exist";
+    Test-Path "$Root/tests/$Type/readme.md" | Should -Be $false -Because "readme.md should not exist";
+    Test-Path "$Root/tests/$Type/project-service-$($suffix[$Type].ToString().ToLowerInvariant())" | Should -Be $true -Because "service folder should not exist";
+  }
+
+  Pop-Location;
+
+  Test-Path "tests/$Type/.idea" | Should -Be $false -Because "JetBrains IDE folders should not exist";
+}
+
+function Start-TemplateBuildTest {
+  param(
+    [string]$Type,
+    [string]$Root
+  )
+
+  Remove-Item -Path "$Root/tests/$Type" -Recurse -Force -ErrorAction Ignore;
+
+  & dotnet new -i "$Root/$Type"
+
+
+  Push-Location $Root;
+  New-Item -Name "tests/$Type" -ItemType "directory" -Force;
+  Pop-Location;
+
+  try {
+    Push-Location "$Root/tests/$Type";
+
+    & dotnet new ion-$Type --project "Project" --service "Service"
+  }
+  finally {
+    Pop-Location;
+  }
+
+  try {
+    Push-Location "$Root/tests/$Type/project-service-$($suffix[$Type].ToString().ToLowerInvariant())/src/Project.Service.$($suffix[$Type])";
+
+    & dotnet restore 
+
+    $LASTEXITCODE | Should -Be 0 -Because "dotnet restore should pass for $Type";
+
+    & dotnet build --no-restore
+
+    $LASTEXITCODE | Should -Be 0 -Because "dotnet build should pass for $Type";
+  }
+  finally {
+    Pop-Location;
+  }
+}
+
+Export-ModuleMember -Function Start-TemplateRenderingTest;
+Export-ModuleMember -Function Start-TemplateBuildTest;
